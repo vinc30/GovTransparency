aec67ab797d081b8031851c06bef445020b86b13
donndonn <vchiang@asu.edu>
2019-11-11 00:34:25 -0500

    modify for parsing text

--- utilities.py
+++ utilities.py
@@ -3,7 +3,11 @@
 import pdfminer
 import pandas as pd
 
-from pdfminer.pdfparser import PDFParser, PDFDocument
+# from pdfminer.pdfparser import PDFParser, PDFDocument
+from pdfminer.pdfparser import PDFParser
+from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdfpage import PDFPage
+
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.converter import PDFPageAggregator
 from pdfminer.layout import LAParams, LTTextBox, LTTextLine
@@ -19,12 +23,12 @@ def extract_text_from_pdf(path):
     source: stackoverflow
     '''
     fp = open(path, 'rb')
-    
+
     parser = PDFParser(fp)
-    doc = PDFDocument()
-    parser.set_document(doc)
-    doc.set_parser(parser)
-    doc.initialize('')
+    doc = PDFDocument(parser)
+    # parser.set_document(doc)
+    # doc.set_parser(parser)
+    # doc.initialize('')
     rsrcmgr = PDFResourceManager()
     laparams = LAParams()
     laparams.char_margin = 1.0
@@ -32,14 +36,14 @@ def extract_text_from_pdf(path):
     device = PDFPageAggregator(rsrcmgr, laparams=laparams)
     interpreter = PDFPageInterpreter(rsrcmgr, device)
     extracted_text = ''
-
-    for page in doc.get_pages():
+    for page in PDFPage.create_pages(doc):
+    # for page in doc.get_pages():
         interpreter.process_page(page)
         layout = device.get_result()
         for lt_obj in layout:
             if isinstance(lt_obj, LTTextBox) or isinstance(lt_obj, LTTextLine):
                 extracted_text += lt_obj.get_text()
-                
+
     return extracted_text
 
 
@@ -47,7 +51,7 @@ def cleanse_df(df):
     '''
     clean the dataframe
 
-    df: `pd.DataFrame` 
+    df: `pd.DataFrame`
     '''
     # strip the space out when the datatype is string
     df = df.applymap(lambda x: x.strip() if isinstance(x, str) else x)
@@ -56,4 +60,3 @@ def cleanse_df(df):
     # blah blah blah
 
     return df
-
